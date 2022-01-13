ae826b9e34c4330901d55cfd377f041d54dfa482
JuMaLaB <julien.malaboeuf@gmail.com>
2022-01-13 14:24:12 +0100

    fix autoconfigure datasource

--- src/main/java/fr/pluralsight/trackzilla/TrackzillaApplication.java
+++ src/main/java/fr/pluralsight/trackzilla/TrackzillaApplication.java
@@ -2,12 +2,13 @@ package fr.pluralsight.trackzilla;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
 public class TrackzillaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TrackzillaApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(TrackzillaApplication.class, args);
+  }
 
 }
--- src/test/java/fr/pluralsight/trackzilla/TrackzillaApplicationTests.java
+++ src/test/java/fr/pluralsight/trackzilla/TrackzillaApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class TrackzillaApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @Test
+  void contextLoads() {
+  }
 
 }
